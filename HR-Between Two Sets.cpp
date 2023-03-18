38f5da4904325f1964307699039146de60ef80ed
Ali Abdullah Jaber Md Bidyuth <117437868+Md-Bidyuth@users.noreply.github.com>
2023-03-19 02:39:33 +0600

    Add files via upload

---
+++ HR-Between Two Sets.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+//#include <boost/math/common_factor.hpp>
+using namespace std;
+int main(){
+    int n,m;
+    cin>>n>>m;
+    int i,arr1[n],arr2[m];
+    for(i=0; i<n; i++)
+    cin>>arr1[n];
+    for(i=0 ; i<m;i++)
+    cin>>arr2[i];
+
+
+
+
+
+
+
+
+
+
+
+
+// x=boost::math::lcm(10,20) ;
+    return 0;
+}
+
