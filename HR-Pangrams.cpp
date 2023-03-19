5cbdbd9974145c41d767c5de3f92c96c51cce131
Ali Abdullah Jaber Md Bidyuth <117437868+Md-Bidyuth@users.noreply.github.com>
2023-03-19 19:01:01 +0600

    Add files via upload

---
+++ HR-Pangrams.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+//#include<string.h>
+
+using namespace std;
+
+int main()
+{
+    string s;
+    getline(cin,s);
+    vector<int>c(26,0);
+    for(int i=0; i<s.size(); i++)
+    {   if(s[i] != ' ')
+        c[tolower(s[i])-'a']++;
+    }
+     string r="pangram";
+     for (int  i=0;i<c.size(); i++ ){
+         if(c[i]==0){
+         r="not "+r;
+         break;
+         }
+     }
+
+     cout <<r;
+    return 0;
+}
+
